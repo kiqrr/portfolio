3f837806ad83e0bb832635563695a669d36841eb
kiqrr <caiquerr99@gmail.com>
2025-01-02 22:11:47 -0300

    preparação da página

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,42 +2,14 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <q-toolbar-title> {KIQ} </q-toolbar-title>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn stretch flat label="Inicio" />
+        <q-btn stretch flat label="Sobre" />
+        <q-btn stretch flat label="Habilidades" />
+        <q-icon name="light_mode" @click="toggleDarkMode" size="1.4rem" />
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -45,58 +17,20 @@
   </q-layout>
 </template>
 
+<style></style>
+
 <script setup>
-import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { useQuasar } from 'quasar'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
-
-const leftDrawerOpen = ref(false)
-
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+const $q = useQuasar()
+// get status
+console.log($q.dark.isActive) // true, false
+// get configured status
+console.log($q.dark.mode) // "auto", true, false
+// set status
+$q.dark.set(true) // or false or "auto"
+// toggle
+function toggleDarkMode() {
+  $q.dark.toggle()
 }
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex flex-center grid-background">
+    <q-separator inset />
   </q-page>
 </template>
 
 <script setup>
 //
 </script>
+
+<style>
+header {
+  padding-inline: 20%;
+}
+
+.grid-background:before {
+  background-image: url(src/assets/grid.svg);
+  background-repeat: repeat;
+  content: '';
+  height: 100%;
+  inset: 0;
+  opacity: 0.2;
+  pointer-events: none;
+  position: absolute;
+  position: fixed;
+  width: 100%;
+  z-index: -1;
+}
+</style>
